2db310b588fffa9eafe6bbdbb55c1e1f6f4afe96
Arthuthu <arthurgeromello@hotmail.com>
2023-08-16 17:05:23 -0300

    Templates removidos add background e descricao da info na index

---
+++ NewPortfolio/Data/Profile/Info.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NewPortfolio.Data.Profile;
+
+public class Info
+{
+	public string Name { get; init; } = "Arthur de Castro Geromello";
+
+	public int Age { get; init; } = GetAge();
+
+
+	public static int GetAge()
+	{
+		DateTime birthDate = new DateTime(1999, 4, 13);
+		DateTime currentDate = DateTime.Now;
+
+		int age = currentDate.Year - birthDate.Year;
+
+		if (currentDate.Month < birthDate.Month || (currentDate.Month == birthDate.Month && currentDate.Day < birthDate.Day))
+		{
+			age--;
+		}
+
+		return age;
+	}
+}
