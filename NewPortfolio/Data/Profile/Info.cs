bbbf27cd7d9825f5063d0728a288f0c023293702
Arthuthu <arthurgeromello@hotmail.com>
2023-08-17 15:05:51 -0300

    Contact section finalized

--- NewPortfolio/Data/Profile/Info.cs
+++ NewPortfolio/Data/Profile/Info.cs
@@ -5,8 +5,11 @@ namespace NewPortfolio.Data.Profile;
 public class Info
 {
 	public string Name { get; init; } = "Arthur de Castro Geromello";
-
 	public int Age { get; init; } = GetAge();
+	public string Number { get; init; } = "(17)99782-4891";
+	public string Email { get; init; } = "arthurgeromello@hotmail.com";
+	public string GithubLink { get; init; } = "https://github.com/Arthuthu";
+	public string LinkedinLink { get; init; } = "https://www.linkedin.com/in/arthur-geromello-ab44a8159/";
 
 
 	public static int GetAge()
