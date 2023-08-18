6a29e6ef792dcc61afea25abf9647f462f7b76b8
Arthuthu <arthurgeromello@hotmail.com>
2023-08-18 14:42:12 -0300

    Add prints do projeto roadmap - Mudança na descrição do projeto da clinica e roadmaps

--- NewPortfolio/Data/Profile/Info.cs
+++ NewPortfolio/Data/Profile/Info.cs
@@ -6,7 +6,7 @@ public class Info
 {
 	public string Name { get; init; } = "Arthur de Castro Geromello";
 	public int Age { get; init; } = GetAge();
-	public string Number { get; init; } = "(17)99782-4891";
+	public string Number { get; init; } = "(17) 99782-4891";
 	public string Email { get; init; } = "arthurgeromello@hotmail.com";
 	public string GithubLink { get; init; } = "https://github.com/Arthuthu";
 	public string LinkedinLink { get; init; } = "https://www.linkedin.com/in/arthur-geromello-ab44a8159/";
